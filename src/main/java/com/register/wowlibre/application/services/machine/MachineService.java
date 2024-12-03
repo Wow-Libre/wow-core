a209d42608f2a9d133d59bdd67f27821b5c60ad7
Sebastian Chitiva <mschitiva68@ucatolica.edu.co>
2024-12-03 02:46:29 -0500

    ajusto probabilidad

--- src/main/java/com/register/wowlibre/application/services/machine/MachineService.java
+++ src/main/java/com/register/wowlibre/application/services/machine/MachineService.java
@@ -42,7 +42,7 @@ public class MachineService implements MachinePort {
 
         final ServerEntity server = verificationDto.server();
 
-        int[] weights = {8, 3, 5, 5, 79};
+        int[] weights = {5, 3, 5, 5, 82};
         String[] outcomes = {"Item", "Level", "Menas", "Gold", "None"};
 
         Optional<MachineEntity> machine = obtainMachine.findByUserIdAndServerId(userId,
