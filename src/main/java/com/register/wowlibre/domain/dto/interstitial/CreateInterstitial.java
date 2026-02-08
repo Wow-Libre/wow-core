6db1ea4507d3d16805e5a8c0223db5d3742dab3e
Sebastian Chitiva <manuchitivar@gmail.com>
2026-02-08 17:31:08 -0500

    correcion de dto

--- src/main/java/com/register/wowlibre/domain/dto/interstitial/CreateInterstitial.java
+++ src/main/java/com/register/wowlibre/domain/dto/interstitial/CreateInterstitial.java
@@ -1,12 +1,15 @@
 package com.register.wowlibre.domain.dto.interstitial;
 
-import jakarta.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Data
 public class CreateInterstitial {
     @NotNull
+    @JsonProperty("urlImg")
     private String urlImg;
     @NotNull
+    @JsonProperty("redirectUrl")
     private String redirectUrl;
 }
